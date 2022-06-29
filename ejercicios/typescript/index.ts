59398c2b2415051070d6fc3f283c006a9a40a356
Pablo <pablogarcirodriguez@gmail.com>
2022-06-29 18:34:01 +0200

    Clase 25, Tipos primitivos en TypeScript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,7 +1,43 @@
-console.log('Hello TypeScript');
+//Boolean
+let muted: boolean = true;
+muted = false;
 
-function add(a: number,b: number){
-    return a + b;
+//Number
+let seis = 6;
+let numerador: number = 42;
+let denominador: number = seis;
+let resultado = numerador/denominador;
+
+//String
+let nombre: string = 'Richard';
+let saludo = `Me llamo ${nombre}`;
+
+//Arrays
+let people: string[] = [];
+people = ['Isable', 'Nicole', 'Raul']
+//people.push(9);
+ 
+let peopleAndNumbers: Array<string | number> = []
+peopleAndNumbers.push('Riacardo');
+peopleAndNumbers.push(98989);
+
+//Enum
+
+enum Color {
+    Rojo = 'rojo',
+    Verde = 'verde',
+    Azul = 'azul',
+    Amarillo = 'amarillo',
 }
 
-const sum = add(2, 3);
+let colorFavorito: Color = Color.Rojo;
+console.log(`Mi color favorito es ${colorFavorito}`);
+
+//Any
+let comodin: any = 'joker';
+comodin = { type: 'Wildcard'};
+comodin = false;
+comodin = 9;
+
+//object
+let someObject: object = { type: 'Wildcard'};
