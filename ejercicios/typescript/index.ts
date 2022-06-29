9cf1a4d56d1ff95641835502017990febb4106c1
Pablo <pablogarcirodriguez@gmail.com>
2022-06-29 18:59:21 +0200

    clase 26, funciones en TypeScript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,43 +1,24 @@
-//Boolean
-let muted: boolean = true;
-muted = false;
-
-//Number
-let seis = 6;
-let numerador: number = 42;
-let denominador: number = seis;
-let resultado = numerador/denominador;
-
-//String
-let nombre: string = 'Richard';
-let saludo = `Me llamo ${nombre}`;
-
-//Arrays
-let people: string[] = [];
-people = ['Isable', 'Nicole', 'Raul']
-//people.push(9);
- 
-let peopleAndNumbers: Array<string | number> = []
-peopleAndNumbers.push('Riacardo');
-peopleAndNumbers.push(98989);
-
-//Enum
-
-enum Color {
-    Rojo = 'rojo',
-    Verde = 'verde',
-    Azul = 'azul',
-    Amarillo = 'amarillo',
+//Funciones
+function add(a: number, b: number): number{
+    return a + b;
 }
 
-let colorFavorito: Color = Color.Rojo;
-console.log(`Mi color favorito es ${colorFavorito}`);
+const sum = add(4, 6);
 
-//Any
-let comodin: any = 'joker';
-comodin = { type: 'Wildcard'};
-comodin = false;
-comodin = 9;
+function createAdder(a: number): (number)=> number{
+    return function(b: number){
+        return b + a;
+    }
+}
 
-//object
-let someObject: object = { type: 'Wildcard'};
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6);
+
+
+function fullName(firstName: string, lastName: string = 'Smith'): string {
+    return `${firstName} ${lastName}`;
+}
+
+const richard = fullName('Richard');
+
+console.log(richard);
